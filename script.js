78c02c124526e1b505a09b0cc79be906eb167ec1
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-12 11:50:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+const methods =Object.getOwnPropertyNames(Math).filter(prop=> typeof Math[prop]==='function');
+	return methods.join(',');
 }
 
 alert(allMethods());
